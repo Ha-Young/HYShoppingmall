d18d466490ea4eed20a114b2785ed22030caeaa8
hychoi <hayeong28@naver.com>
2020-05-05 12:30:34 +0900

    create class based view and register form

---
+++ hy_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hychoi/Documents/Projects/HYShoppingmall/hy_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- hy_shopping/hyuser/forms.py
+++ hy_shopping/hyuser/forms.py
@@ -1,16 +1,55 @@
 from django import forms
+from .models import Hyuser
+from django.contrib.auth.hashers import check_password, make_password
+
 
 class RegisterForm(forms.Form):
     email = forms.EmailField(
-        label="email",
+        label="이메일",
         required=True, 
         error_messages={
             'required': '이메일을 입력하십시오'
-        })
+        }
+    )
+
     password = forms.CharField(
-        label="password",
+        label="비밀번호",
         required=True, 
         error_messages={
             'required': '비밀번호를 입력하십시오'
         },
-        widget=forms.PasswordInput)
+        widget=forms.PasswordInput
+    )
+
+    re_password = forms.CharField(
+        label="비밀번호 확인",
+        required=True, 
+        error_messages={
+            'required': '비밀번호를 입력하십시오'
+        },
+        widget=forms.PasswordInput
+    )
+
+    def clean(self):
+        print("clean")
+        cleaned_data = super().clean()
+        
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+        re_password = cleaned_data.get('re_password')
+
+        print(password, re_password)
+
+        if password and re_password:
+            if password != re_password:
+                print("not same")
+                self.add_error('password', '비밀번호가 서로 다릅니다')
+                self.add_error('re_password', '비밀번호가 서로 다릅니다')
+                return
+            else:
+                new_hyuser = Hyuser(
+                                email=email,
+                                password=make_password(password)
+                )
+
+                new_hyuser.save()
