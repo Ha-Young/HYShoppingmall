902dd9cac405e92ccb731613c33ecaab8072e966
hychoi <hayeong28@naver.com>
2020-05-13 22:49:03 +0900

    make login && check valid

--- hy_shopping/hyuser/forms.py
+++ hy_shopping/hyuser/forms.py
@@ -53,3 +53,48 @@ class RegisterForm(forms.Form):
                 )
 
                 new_hyuser.save()
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        label="이메일",
+        required=True, 
+        error_messages={
+            'required': '이메일을 입력하십시오'
+        }
+    )
+
+    password = forms.CharField(
+        label="비밀번호",
+        required=True, 
+        error_messages={
+            'required': '비밀번호를 입력하십시오'
+        },
+        widget=forms.PasswordInput
+    )
+
+    def clean(self):
+        print("clean")
+        cleaned_data = super().clean()
+        
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        print(email, password)
+
+        if email and password:
+            try:
+                hyuser = Hyuser.objects.get(email=email)
+            except Hyuser.DoseNotExist as e:
+                print(e)
+                self.add_error('email', '없는 email 입니다')
+                return
+            except Exception as e:
+                print(e)
+                self.add_error('email', '알수없는 오류')
+                return
+
+            if not check_password(password, hyuser.password):
+                self.add_error('password', '비밀번호를 틀렸습니다')
+            else:
+                self.user_id = hyuser.id
