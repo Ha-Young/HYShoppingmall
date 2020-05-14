54779a7c855f0525d00b6acff04f43a1ae65aa9c
hychoi <hayeong28@naver.com>
2020-05-15 01:57:28 +0900

    order logic complete

--- hy_shopping/order/forms.py
+++ hy_shopping/order/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import Order
+from product.models import Product
+from hyuser.models import Hyuser
 
 
 class RegisterForm(forms.Form):
+
+    def __init__(self, request, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request = request
+
     quantity = forms.IntegerField(
         label="수량",
         required=True, 
@@ -26,4 +33,17 @@ class RegisterForm(forms.Form):
         
         quantity = cleaned_data.get('quantity')
         product = cleaned_data.get('product')
-        
+        hyuser = self.request.session.get('user')
+
+        if quantity and product and hyuser:
+            order = Order(
+                quantity=quantity,
+                product=Product.objects.get(pk=product),
+                hyuser=Hyuser.objects.get(pk=hyuser)
+            )
+
+            order.save()
+        else:
+            self.product = product
+            self.add_error('quantity', '값이 없습니다')
+            self.add_error('product', '값이 없습니다')
