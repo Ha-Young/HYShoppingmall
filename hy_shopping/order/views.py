d18eebb5ccd167abc03880d75e3479ca05fc7e79
hychoi <hayeong28@naver.com>
2020-05-27 01:16:43 +0900

    get api data in orderlist(listview)

--- hy_shopping/order/views.py
+++ hy_shopping/order/views.py
@@ -12,7 +12,7 @@ from .serializers import OrderSerializer
 from rest_framework import generics
 from rest_framework import mixins
 from . import query
-
+import requests
 # Create your views here.
 
 class OrderListAPI(generics.GenericAPIView, mixins.ListModelMixin):
@@ -93,8 +93,14 @@ class OrderCreate(FormView):
 @method_decorator(login_required, name='dispatch')
 class OrderList(ListView):
     template_name = 'order.html'
-    context_object_name = 'order_list'
+    # context_object_name = 'order_list'
 
-    def get_queryset(self, **kwargs):
-        queryset = Order.objects.filter(hyuser__id=self.request.session.get('user'))
-        return queryset
+    # def get_queryset(self, **kwargs):
+    #     queryset = Order.objects.filter(hyuser__id=self.request.session.get('user'))
+    #     return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(OrderList, self).get_context_data(**kwargs)
+        order_list = requests.get('http://127.0.0.1:8000/api/order/?product=1').json()
+        context['order_list'] = order_list
+        return context 
